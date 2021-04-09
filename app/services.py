bd5c75d576193d60358db8f6cb129ebd3591c364
Dmitriy Zharkov <zharkov.de@edu.spbstu.ru>
2021-04-09 19:31:44 +0300

    Проверил

--- app/services.py
+++ app/services.py
@@ -2,9 +2,9 @@ from app.DAO_classes import TechTaskDAO
 
 
 class TechTaskService:
-    def addTask(self, task_id):
+    def add_task(self, task_id):
         if task_id is not None:
             TechTaskDAO.create(task_id)
 
-    def deleteTask(self, task_id):
+    def delete_task(self, task_id):
         TechTaskDAO.delete(task_id)
