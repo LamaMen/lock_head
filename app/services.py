6534df706691f1b99d2af4899b9eaa308824ad35
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-04-09 19:30:54 +0300

    Исправил импорты

--- app/services.py
+++ app/services.py
@@ -1,10 +1,10 @@
-from app import DAO_classes
+from app.DAO_classes import TechTaskDAO
 
 
 class TechTaskService:
     def addTask(self, task_id):
         if task_id is not None:
-            DAO_classes.TechTaskDAO.create(task_id)
+            TechTaskDAO.create(task_id)
 
     def deleteTask(self, task_id):
-        DAO_classes.TechTaskDAO.delete(task_id)
+        TechTaskDAO.delete(task_id)
