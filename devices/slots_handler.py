21ce9e289a21a10ca211158799af4ee83d558bd5
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-04-12 10:13:22 +0300

    Подготовил в выгрузке

--- devices/slots_handler.py
+++ devices/slots_handler.py
@@ -1,11 +1,11 @@
-from smbus import SMBus
+# from smbus import SMBus
 
 ARDUINO_I2C_ADDRESS = 0x8
 
 
 def open_slot(slot_number):
-    bus = SMBus(1)
-    bus.write_byte(ARDUINO_I2C_ADDRESS, slot_number)
-    bus.close()
+    # bus = SMBus(1)
+    # bus.write_byte(ARDUINO_I2C_ADDRESS, slot_number)
+    # bus.close()
 
-    # print('open slot ' + str(slot_number))
+    print('open slot ' + str(slot_number))
