c77e2f3897d329bbc5f2dd3caaa05c40cc409027
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-05-11 23:38:53 +0300

    Попробавл решить проблему с закрытием ячейки

--- devices/slots_handler.py
+++ devices/slots_handler.py
@@ -1,10 +1,18 @@
 from smbus import SMBus
+from datetime import datetime
 
 ARDUINO_I2C_ADDRESS = 0x8
+last_open = datetime(1970, 1, 1)
 
 
 def open_slot(slot_number):
-    bus = SMBus(1)
-    bus.write_byte(ARDUINO_I2C_ADDRESS, slot_number)
-    bus.close()
+    global last_open
+    now = datetime.now()
+
+    if (now - last_open).seconds > 5:
+        last_open = now
+        bus = SMBus(1)
+        bus.write_byte(ARDUINO_I2C_ADDRESS, slot_number)
+        bus.close()
+
     # print('open slot ' + str(slot_number))
